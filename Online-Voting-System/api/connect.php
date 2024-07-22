f58bf6543cf5cecd2a7e57a2234b4cee20d2909a
Ujjawal Tyagi <111866185+Ujjawalty@users.noreply.github.com>
2024-07-22 21:48:25 +0530

    Add files via upload

---
+++ Online-Voting-System/api/connect.php
@@ -0,0 +1,12 @@
+<!-- In this code we give the connectivity -->
+
+<?php
+$connect =mysqli_connect("localhost","root","","voting") or die("connection failed!!"); //This code give the connectivity for php to database
+
+// if($connect){
+//     echo "connected !!";
+// }
+// else{
+//     echo "not connected";
+// }
+?>
